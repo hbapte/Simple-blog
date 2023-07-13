c2c0e49bfe3ebaddb0e93ef149dccab3e6e6e792
hbapte <ijbapte@gmail.com>
2023-07-14 00:46:03 +0200

    reusable component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-color-red'>
+    <footer className='text-center mt-2 '> hbapte &copy;2023 </footer>
+    </div>
+  )
+}
+
+export default Footer
